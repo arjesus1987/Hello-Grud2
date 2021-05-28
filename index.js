96aad4014c0a80b3c2d4c89b189cb0e00d2d3033
arjesus1987 <alan.rabelodejesus@gmail.com>
2021-05-27 22:12:24 -0300

    Versaão 27/05/21 II

--- index.js
+++ index.js
@@ -16,6 +16,38 @@ app.get ('/usuarios', (req, res) => {
     res.json(usuarios);
 });
 
+app.post('/usuarios', (req, res) => {
+    const {nome, idade} = req.body;
+    const id = usuarios.length;
+
+    const novoUsuario = {
+        id,
+        nome,
+        idade
+    }
+    usuarios.push(novoUsuario);
+    res.status(201).json(novoUsuario);
+});
+
+app.put('/usuarios/:id', (req, res) => {
+    const {id} = req.params;
+    const {nome, idade} = req.body;
+
+    const usuario = usuarios.find(usuario => usuario.id === Number(id));
+    usuario.nome = nome;
+    usuario.idade = idade;
+
+    res.json(usuarios);
+});
+
+app.delete('/usuarios/:id', (req, res) => {
+    const {id} = req.params;
+    const index = usuarios.findIndex (usuario => usuario.id === Number(id));
+    
+    usuarios.splice(index,1);
+    res.status(204).send();
+});
+
 app.listen(port, () => {
     console.log(`Server is up and rumnig on port ${port}`);
 });
